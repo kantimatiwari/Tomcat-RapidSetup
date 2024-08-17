051f328c98c78006b6dc98a56d4692ea7a57cb5c
Kantima Tiwari <131696304+kantimatiwari@users.noreply.github.com>
2024-08-18 00:19:46 +0530

    Create portuner-tomcat.sh

---
+++ portuner-tomcat.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+# Prompt the user to enter a new port number
+echo "Enter new port number (1024-65535): "
+read CUSTOM_TOMCAT_PORT
+
+# Update the port number in server.xml
+sudo sed -i ' /<Connector port/  c \ \ \ \ <Connector port="'$CUSTOM_TOMCAT_PORT'" protocol="HTTP/1.1" '  /opt/tomcat/conf/server.xml
+
+# Update the portnumber in tomcatcreds.txt
+sudo sed -i '4 c portnumber='$CUSTOM_TOMCAT_PORT' ' /opt/tomcreds.txt
+
+echo "Port number successfully updated to "'$CUSTOM_TOMCAT_PORT'". "
+echo "Restart tomcat (comm: tomcat --restart) to apply chnages"
